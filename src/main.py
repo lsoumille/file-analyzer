d1947576676f53d070e7e56d3ff7cf27565e1068
lsoumille <lucas.soumille@etu.unice.fr>
2017-12-29 09:35:06 +0100

    getopt for file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,27 @@
+import getopt
+import sys
+
+
 def main():
-    print("Hello")
+    file_path = ""
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hf", ["help", "file="])
+    except getopt.GetoptError:
+        #usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            #usage()
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            file_path = arg
+        else:
+            assert False, "unhandled option"
+
+    if not file_path:
+        print("File is needed")
+        sys.exit(2)
+
+
 
 if  __name__ =='__main__':main()
